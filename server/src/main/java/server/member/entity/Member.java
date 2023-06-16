a744b0b1d4de63e6feb8387dd8d4a55ca0a7eee9
johyeran <a01026823164@gmail.com>
2023-06-16 11:49:19 +0900

    feat: member 엔티티 추가

---
+++ server/src/main/java/server/member/entity/Member.java
@@ -0,0 +1,17 @@
+package server.member.entity;
+
+import javax.persistence.*;
+
+@Table
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false, unique = true)
+    private String alias;
+}
