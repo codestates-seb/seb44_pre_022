06a2f9f0465e26259b3e057b8c9a00bce6c9ab47
baejb <bjb6478@naver.com>
2023-06-19 14:10:42 +0900

    feat: 질문작성페이지 구현 완료

--- client/src/pages/AskQuestionPage.tsx
+++ client/src/pages/AskQuestionPage.tsx
@@ -4,18 +4,28 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import AskQuestion from '../components/AskQuestion';
 
-const HomeContainer = tw.div`
+const Container = tw.div`
 bg-white
 flex flex-col justify-start items-center
 `;
 
+const MiddleContainer = tw.div`
+mt-[90px]
+max-w-[1410px] w-[100%] h-full
+flex
+
+`;
+
 const AskQuestionPage = () => {
   return (
-    <HomeContainer>
+    <Container>
       <Header />
-      <AskQuestion />
+
+      <MiddleContainer>
+        <AskQuestion />
+      </MiddleContainer>
       <Footer />
-    </HomeContainer>
+    </Container>
   );
 };
 
