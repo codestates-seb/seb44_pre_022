b73bce499c582b9d4c6689dd28f7a3c54159cb3f
WAYPIL <ohs3051009@gmail.com>
2023-06-20 00:33:48 +0900

    Revert "Feat/recover-missing-file: Signup.tsx & SignupPage.tsx"

--- server/src/main/java/server/member/controller/MemberController.java
+++
@@ -1,9 +0,0 @@
-package server.member.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/member")
-public class MemberController {
-}
