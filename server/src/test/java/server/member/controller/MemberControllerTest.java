23b3df5b956a999e86bed71f804e03c48f4dff69
johyeran <a01026823164@gmail.com>
2023-06-20 14:31:16 +0900

    fix: member test 코드 수정 진행

--- server/src/test/java/server/member/controller/MemberControllerTest.java
+++ server/src/test/java/server/member/controller/MemberControllerTest.java
@@ -1,36 +1,42 @@
 package server.member.controller;
 
+import com.google.gson.Gson;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 import server.member.repository.dto.MemberDto;
 import server.member.entity.Member;
 import server.member.mapper.MemberMapper;
 import server.member.service.MemberService;
 
+@Slf4j
 @SpringBootTest
 @AutoConfigureMockMvc
 public class MemberControllerTest {
-  @Autowired
+  @MockBean
   private MemberService memberService;
-  @Autowired
+
+  @MockBean
   private MemberMapper mapper;
+  @Autowired
+  private MockBean mockBean;
+
+  @Autowired
+  private Gson gson;
 
   @Test
-  @DisplayName("비밀번호 유효성 검증 실패 테스트")
-  public void postMemberExample01() {
-    MemberDto.Post memberDto = new MemberDto.Post("test@gmail.com", "asdf", "test");
-    mapper.memberPostDtoToMember(memberDto);
-  }
-  @Test
-  @DisplayName("비밀번호 유효성 검증 성공 테스트")
-  public void postMemberExample02() {
-    MemberDto.Post memberDto = new MemberDto.Post("test2@gmail.com", "asdf1234!", "test");
+  private void postMember() {
+    // given
 
-    Member member = mapper.memberPostDtoToMember(memberDto);
 
-    memberService.createMember(member);
+    // when
+
+    // then
   }
+
 }
--- server/src/test/java/server/member/service/MemberServiceTest.java
+++ server/src/test/java/server/member/service/MemberServiceTest.java
@@ -1,5 +1,6 @@
 package server.member.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,16 +11,25 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataIntegrityViolationException;
 import server.member.entity.Member;
+import server.member.mapper.MemberMapper;
 import server.member.repository.MemberRepository;
+import server.member.repository.dto.MemberDto;
 
 import javax.transaction.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@Slf4j
 @ExtendWith(MockitoExtension.class)
 public class MemberServiceTest {
   @Autowired
   private MemberRepository memberRepository;
+
+  @Autowired
+  private MemberService memberService;
+
+  @Autowired
+  private MemberMapper mapper;
 
   @Test
   @DisplayName("중복 테스트")
@@ -36,4 +46,21 @@ public class MemberServiceTest {
 
     assertThrows(DataIntegrityViolationException.class, () -> memberRepository.save(member2));
   }
+  /*@Test
+  @DisplayName("비밀번호 유효성 검증 실패 테스트")
+  public void postMemberExample01() {
+    MemberDto.Post memberDto = new MemberDto.Post("test@gmail.com", "asdf", "test");
+    Member member = mapper.memberPostDtoToMember(memberDto);
+    memberService.createMember(member);
+  }
+
+  @Test
+  @DisplayName("비밀번호 유효성 검증 성공 테스트")
+  public void postMemberExample02() {
+    MemberDto.Post memberDto = new MemberDto.Post("test2@gmail.com", "asdf1234!", "test");
+
+    Member member = mapper.memberPostDtoToMember(memberDto);
+
+    memberService.createMember(member);
+  }*/
 }
