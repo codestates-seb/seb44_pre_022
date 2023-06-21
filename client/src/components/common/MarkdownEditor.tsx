9fcc62f8bab7ea2a36986fe5dc093722f7b61d0d
WAYPIL <ohs3051009@gmail.com>
2023-06-21 11:52:30 +0900

    Rename: value → markdownText

--- client/src/components/common/MarkdownEditor.tsx
+++ client/src/components/common/MarkdownEditor.tsx
@@ -5,13 +5,13 @@ import MDEditor from '@uiw/react-md-editor';
 const MDEditorContainer = tw.section`flex flex-col p-[30px]`;
 
 export default function MarkdownEditorComponent() {
-  const [value, setValue] = React.useState<string | undefined>(
+  const [markdownText, setMarkdownText] = React.useState<string | undefined>(
     '이곳에 글을 작성해주세요.'
   );
 
   return (
     <MDEditorContainer data-color-mode='light'>
-      <MDEditor value={value} onChange={setValue} />
+      <MDEditor value={markdownText} onChange={setMarkdownText} />
       {/* <MDEditor.Markdown source={value} style={{ whiteSpace: 'pre-wrap' }} /> */}
     </MDEditorContainer>
   );
