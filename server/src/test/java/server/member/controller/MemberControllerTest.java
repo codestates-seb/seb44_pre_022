a6fd62d1337c0f4d3ec658779085d309e2531241
johyeran <a01026823164@gmail.com>
2023-06-22 11:36:50 +0900

    feat: jwt 라이브러리 추가 및 member test 코드 추가

--- server/src/test/java/server/member/controller/MemberControllerTest.java
+++ server/src/test/java/server/member/controller/MemberControllerTest.java
@@ -15,6 +15,8 @@ import server.member.repository.dto.MemberDto;
 import server.member.mapper.MemberMapper;
 import server.member.service.MemberService;
 
+import java.util.List;
+
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.startsWith;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
@@ -53,9 +55,75 @@ public class MemberControllerTest {
   }
 
   @Test
-  public void pathMember() {
+  public void pathMember() throws Exception {
     // given
+    MemberDto.Patch memberDto = new MemberDto.Patch(1, "test@gmail.com", "test1234!", "test");
+    String content = gson.toJson(memberDto);
     // when
+    ResultActions actions = mockMvc.perform(
+            MockMvcRequestBuilders
+                    .patch("/member/1")
+                    .accept(MediaType.APPLICATION_JSON)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(content)
+    );
     // then
+    actions.andExpect(
+            status().isOk()
+    );
+  }
+  @Test
+  public void getMember() throws Exception {
+    // given
+    MemberDto.Response response = new MemberDto.Response(1, "test@gmail.com", "test1234!", "테스트");
+    String content = gson.toJson(response);
+
+    // when
+    ResultActions actions = mockMvc.perform(
+            MockMvcRequestBuilders.get(
+                            "/member/1"
+                    )
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(content)
+    );
+    // then
+    actions.andExpect(
+            status().isOk()
+    );
+  }
+  @Test
+  public void getMembers() throws Exception {
+    // given
+    List<MemberDto.Response> response = List.of(
+            new MemberDto.Response(1, "test@gmail.com", "test1234!", "테스트"),
+            new MemberDto.Response(2, "test2@gmail.com", "test1234!", "테스트2")
+    );
+    String content = gson.toJson(response);
+
+    // when
+    ResultActions actions = mockMvc.perform(
+            MockMvcRequestBuilders.get(
+                            "/member"
+                    )
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(content)
+    );
+    // then
+    actions.andExpect(
+            status().isOk()
+    );
+  }
+  @Test
+  public void deleteMember() throws Exception {
+    // when
+    ResultActions actions = mockMvc.perform(
+            MockMvcRequestBuilders.delete(
+                            "/member/1"
+                    )
+    );
+    // then
+    actions.andExpect(
+            status().isNoContent()
+    );
   }
 }
