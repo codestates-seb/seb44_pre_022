2b6b36d25e3c90e2ff871d0fe4628457d5d7e4b7
Seo Jaegon <jgoneit@gmail.com>
2023-06-22 14:56:09 +0900

    feat: 투표 기능 아키텍처 생성

--- server/src/main/java/server/vote/entity/Vote.java
+++ server/src/main/java/server/vote/entity/Vote.java
@@ -1,2 +1,28 @@
-package server.vote.entity;public class Vote {
+package server.vote.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import server.member.entity.Member;
+import server.question.entity.Question;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import static javax.persistence.GenerationType.*;
+
+@Getter
+@Setter
+@Entity
+public class Vote {
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  private Long voteId;
+
+  @ManyToOne
+  private Member member;
+
+  @ManyToOne
+  private Question question;
 }
