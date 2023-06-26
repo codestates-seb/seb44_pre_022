032fe2c79f14fe535b2553a61c8705a5ddaee4a4
johyeran <a01026823164@gmail.com>
2023-06-26 17:43:49 +0900

    feat: security member role 추가

---
+++ server/src/main/java/server/auth/utils/CustomAuthUtils.java
@@ -0,0 +1,29 @@
+package server.auth.utils;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class CustomAuthUtils {
+    @Value("${mail.admin.address}")
+    private String ADMIN_EMAIL;
+
+    private final List<GrantedAuthority> ADMIN_ROLES = AuthorityUtils.createAuthorityList("ADMIN_ROLE", "USER_ROLE");
+    private final List<GrantedAuthority> USER_ROLES = AuthorityUtils.createAuthorityList("USER_ROLE");
+
+    private final List<String> ADMIN_ROLES_STR = List.of("ADMIN", "USER");
+    private final List<String> USER_ROLES_STR = List.of("USER");
+
+    public List<String> createRoles(String email) {
+        if(email.equals(ADMIN_EMAIL)) {
+            return ADMIN_ROLES_STR;
+        }
+        return USER_ROLES_STR;
+    }
+}
